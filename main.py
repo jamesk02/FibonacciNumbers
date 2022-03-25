07e5d397057aa3004adb7b246b01c6fe2956e803
James Kirkham <james.kirkham-4@student.manchester.ac.uk>
2022-03-25 15:42:49 +0000

    Iterative implementation fixed bug, similar TC and SC within low range, however doesn't cause a recursion error allowing it to compute much higher fibonaccis

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
 
     fib_nums = [0, 1]
 
-    for x in range(2, n):
+    for x in range(2, n + 1):
         fib_nums.append(fib_nums[x - 1] + fib_nums[x - 2])
         0
 
-    print(fib_nums[n - 1])
-    
+    print(fib_nums[n])
+    0
