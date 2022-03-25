cf5a68b2e71d707cda1feac93d2020ac8a710962
James Kirkham <james.kirkham-4@student.manchester.ac.uk>
2022-03-25 15:30:50 +0000

    Fast recursive implementation

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
+fib_nums = dict({0: 0, 1: 1})
 
-    return fibonacci(n - 1) + fibonacci(n - 2)
+
+def fibonacci(n):
+    # Checks if what we need has already been computed
+    # if so then simply return that value
+    if n in fib_nums:
+        return fib_nums.get(n)
+
+    # Stores the value in the dictionary so we don't have to
+    # recalculate when calculating other fibs that use this
+    fib_nums[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    return fib_nums[n]
 
 
 if __name__ == "__main__":
